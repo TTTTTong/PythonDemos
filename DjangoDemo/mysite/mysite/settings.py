084fd8415cddc313ee871f2ede5370ec3ae1983b
tong <t15529208705@gmail.com>
2017-12-12 00:05:08 +0800

    blog_tags

--- DjangoDemo/mysite/mysite/settings.py
+++ DjangoDemo/mysite/mysite/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'blog_tags': 'blog.templatetags.blog_tags',
+            }
         },
     },
 ]
