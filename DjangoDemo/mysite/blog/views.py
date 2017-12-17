4384e524d3a5129dbebb91ba040b8e516e11d784
tong <t15529208705@gmail.com>
2017-12-18 00:11:10 +0800

    class views

--- DjangoDemo/mysite/blog/views.py
+++ DjangoDemo/mysite/blog/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
+from django.views.generic import ListView
 from .models import Post, Category
 from comments.forms import CommentForm
 import markdown
 import pygments
 
-# Create your views here.
 
-
-def index(request):
-    title = 'the index'
-    welcome = 'welcome'
-    post_list = Post.objects.all().order_by('-create_time')
-    return render_to_response('blog/index.html', locals())
+# def index(request):
+#     title = 'the index'
+#     welcome = 'welcome'
+#     post_list = Post.objects.all().order_by('-create_time')
+#     return render_to_response('blog/index.html', locals())
 
 
 def detail(request, pk):
@@ -27,13 +26,35 @@ def detail(request, pk):
     return render_to_response('blog/detail.html', locals())
 
 
-def archives(request, year, month):
-    # 由于mysql的时区设置，要在setting.py中把USE_TZ改为False
-    post_list = Post.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
-    return render_to_response('blog/index.html', locals())
+# def archives(request, year, month):
+#     # 由于mysql的时区设置，要在setting.py中把USE_TZ改为False
+#     post_list = Post.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
+#     return render_to_response('blog/index.html', locals())
 
 
-def categories(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=category)
-    return render_to_response('blog/index.html', locals())
+# def categories(request, pk):
+#     category = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=category)
+#     return render_to_response('blog/index.html', locals())
+
+
+# -----------------------------------------------------------
+# 使用类视图改写视图函数
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+
+class CategoriesView(IndexView):
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoriesView, self).get_queryset().filter(category=category)
+
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(create_time__year=year,
+                                                               create_time__month=month)
