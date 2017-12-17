cde3df9abc5732731a6232cceac17659f450d075
tong <t15529208705@gmail.com>
2017-12-17 23:43:23 +0800

    auto geneExcerpt

--- DjangoDemo/mysite/blog/models.py
+++ DjangoDemo/mysite/blog/models.py
@@ -44,24 +44,24 @@ class Post(models.Model):
 
     # 自动生成摘要
     def save(self, *args, **kwargs):
-        if not self.excerpt:
-            self.excerpt = self.body[:50]
-            print(self.body[:50])
-        super(Post, self).save(*args, **kwargs)
-
         # if not self.excerpt:
-        #     # 首先实例化一个 Markdown 类，用于渲染 body 的文本
-        #     md = markdown.Markdown(extensions=[
-        #         'markdown.extensions.extra',
-        #         'markdown.extensions.codehilite',
-        #     ])
-        #     # 先将 Markdown 文本渲染成 HTML 文本
-        #     # strip_tags 去掉 HTML 文本的全部 HTML 标签
-        #     # 从文本摘取前 54 个字符赋给 excerpt
-        #     self.excerpt = strip_tags(md.convert(self.body))[:54]
-        #
-        #     # 调用父类的 save 方法将数据保存到数据库中
+        #     self.excerpt = self.body[:50]
+        #     print(self.body[:50])
         # super(Post, self).save(*args, **kwargs)
+
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+            # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-create_time']
