256272e723cb940160b43b741930a5fae4b0e65f
tong <t15529208705@gmail.com>
2018-06-27 00:21:52 +0800

    2018.6.26

--- z_work/query_daemon2.py
+++ z_work/query_daemon2.py
@@ -14,8 +14,6 @@ logging.config.fileConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)
 logger = logging.getLogger('monitor')
 logger.info('start monitor daemon')
 
-# 多线程版本, ticker、kline、trade、depth各占一个线程,某项重启等待时不影响其他项
-
 
 def get_mysqlcur():
     conn = MySQLdb.connect(host='rm-j6ccb77fan1q3p0n8.mysql.rds.aliyuncs.com',
@@ -25,13 +23,13 @@ def get_mysqlcur():
 
 
 # 从数据库获取各项最新值
-def query_ticker(exchange_id, symbol):
+def query_ticker(exchange_id):
     cur_ticker = get_mysqlcur()
-    sql = "SELECT `close` FROM `market_ticker` WHERE `exchange_id` = %s AND `symbol` = %s"
-    cur_ticker.execute(sql, args=(exchange_id, symbol))
+    sql = "SELECT `time` FROM `market_ticker` WHERE `exchange_id` =%s ORDER BY `time` DESC LIMIT 1"
+    cur_ticker.execute(sql, args=(exchange_id,))
     res = cur_ticker.fetchone()
     if res is None:
-        logger.info('query_ticker is None, return 0, exchanged_id is:%s,symbol is:%s' % (exchange_id, symbol))
+        logger.info('query_ticker is None, return 0, exchanged_id is:%s' % exchange_id)
         res = 0
     else:
         res = res[0]
@@ -39,14 +37,13 @@ def query_ticker(exchange_id, symbol):
     return res
 
 
-def query_kline(excahnge_id, symbol):
+def query_kline(excahnge_id):
     cur_kline = get_mysqlcur()
-    sql = "SELECT `time` FROM `market_kline` WHERE `exchange_id`=%s AND `symbol`=%s AND" \
-          " `period`=1 ORDER BY `time` DESC LIMIT 1"
-    cur_kline.execute(sql, args=(excahnge_id, symbol))
+    sql = "SELECT  `time` from `market_kline`  WHERE `exchange_id` =%s AND `period` =1 ORDER BY `time` DESC LIMIT 1 "
+    cur_kline.execute(sql, args=(excahnge_id,))
     res = cur_kline.fetchone()
     if res is None:
-        logger.info('query_kline is None, return 0, exchanged_id is:%s,symbol is:%s' % (excahnge_id, symbol))
+        logger.info('query_kline is None, return 0, exchanged_id is:%s' % excahnge_id)
         res = 0
     else:
         res = res[0]
@@ -54,14 +51,13 @@ def query_kline(excahnge_id, symbol):
     return res
 
 
-def query_trade(exchange_id, symbol):
+def query_trade(exchange_id):
     cur_trade = get_mysqlcur()
-    sql = "SELECT `time` FROM `market_history_trade` WHERE `exchange_id`=%s AND " \
-          "`symbol`=%s ORDER BY `time` DESC LIMIT 1"
-    cur_trade.execute(sql, args=(exchange_id, symbol))
+    sql = "SELECT `time` FROM `market_history_trade` WHERE `exchange_id`=%s ORDER BY `time` DESC LIMIT 1"
+    cur_trade.execute(sql, args=(exchange_id,))
     res = cur_trade.fetchone()
     if res is None:
-        logger.info('query_trade is None, return 0, exchanged_id is:%s,symbol is:%s' % (exchange_id, symbol))
+        logger.info('query_trade is None, return 0, exchanged_id is:%s' % exchange_id)
         res = 0
     else:
         res = res[0]
@@ -85,130 +81,145 @@ def restart(name):
 
 # 将时间戳转换为(时:分:秒)
 def format_time(timestamp):
-    return time.strftime('%H:%M:%S', time.localtime(timestamp))
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
 
 
 # 各项的处理函数, 决定是否重启对应服务
 # ticket
-def ticker_handle():
-    result_dict = {}
-    init_result = [0, 0, 1]
-    for item in item_list:
-        # 为每个交易所的每个交易对保存查询结果
-        # 每个结果为包含三个元素的list，三次一样则重启，[0, 0, 1]为初始指定值
-        result_dict[item[1]+'_'+item[2]] = init_result
-
+def ticker_handle(exchange_id, exchange):
     while True:
         is_restart = False
-        for item in item_list:
-            # 每个交易所的每个交易对的三次查询结果
-            symbol_rst = result_dict[item[1]+'_'+item[2]]
-            symbol_rst[0], symbol_rst[1] = symbol_rst[1], symbol_rst[2]
-            symbol_rst[2] = query_ticker(item[0], item[1])
-            logger.info('item is: %s,last three results: %s, %s, %s' % (str(item[0])+'_'+str(item[1])+'_'+str(item[2]),
-                                                                       symbol_rst[0], symbol_rst[1], symbol_rst[2]))
-            if symbol_rst[0] == symbol_rst[1] == symbol_rst[2]:
-                restart(item[2] + '_ticker')
-                logger.info('begin sleep 120s after restart %s_ticket' % item[2])
-                time.sleep(120)
-                is_restart = True
-                logger.info('finish sleep 120s after restart %s_ticket' % item[2])
-        if not is_restart:    
-            logger.info('all ticker is normal, sleep 30s before next query' )
+        cur_timestamp = int(time.time())
+        db_timestamp = query_ticker(exchange_id)
+        logger.info('item is: %s, current timestamp:%s (%s) , db timestamp:%s (%s), ' %
+                        (exchange+'_ticker', cur_timestamp, format_time(cur_timestamp),
+                            db_timestamp, format_time(db_timestamp)))
+        if cur_timestamp - db_timestamp > 300:
+            restart(exchange + '_ticker')
+            logger.info('begin sleep 120s after restart %s_ticker' % exchange)
+            time.sleep(120)
+            is_restart = True
+            # logger.info('finish sleep 120s after restart %s_trade' % item[2])
+        if not is_restart:
+            logger.info('all ticker is normal, sleep 30s before next query')
             time.sleep(30)
 
 
 # kline
-def kline_handle():
+def kline_handle(exchange_id, exchange):
     while True:
         is_restart = False
-        for item in item_list:
-            cur_timestamp = int(time.time())
-            db_timestamp_kline = query_kline(item[0], item[1])
-            logger.info('item is: %s, current timestamp:%s (%s) , db timestamp:%s (%s), ' %
-                            (str(item[0])+'_'+str(item[1]+'_'+item[2]), cur_timestamp, format_time(cur_timestamp),
-                            db_timestamp_kline, format_time(db_timestamp_kline)))
-            if cur_timestamp - db_timestamp_kline > 120:
-                restart(item[2] + '_kline')
-                logger.info('begin sleep 300s after restart %s_kline' % item[2])
-                time.sleep(300)
-                logger.info('finish sleep 300s after restart %s_kline' % item[2])
-                is_restart = True
+        cur_timestamp = int(time.time())
+        db_timestamp = query_kline(exchange_id)
+        logger.info('item is: %s, current timestamp:%s (%s) , db timestamp:%s (%s), ' %
+                        (exchange+'_kline', cur_timestamp, format_time(cur_timestamp),
+                        db_timestamp, format_time(db_timestamp)))
+        if cur_timestamp - db_timestamp > 300:
+            restart(exchange + '_kline')
+            logger.info('begin sleep 300s after restart %s_kline' % exchange)
+            time.sleep(300)
+            # logger.info('finish sleep 300s after restart %s_kline' % item[2])
+            is_restart = True
         if not is_restart:
             logger.info('all kline is normal, sleep 60s before next query')
             time.sleep(60)
 
 
 # trade
-def trade_handle():
+def trade_handle(exchange_id, exchange):
     while True:
         is_restart = False
-        for item in item_list:
-            cur_timestamp_2 = int(time.time())
-            db_timestamp_trade = query_trade(item[0], item[1])
-            logger.info('item is: %s, current timestamp:%s (%s) , db timestamp:%s (%s), ' %
-                            (str(item[0])+'_'+str(item[1]+'_'+item[2]), cur_timestamp_2, format_time(cur_timestamp_2),
-                                db_timestamp_trade, format_time(db_timestamp_trade)))
-            if cur_timestamp_2 - db_timestamp_trade > 120:
-                restart(item[2] + '_trade')
-                logger.info('begin sleep 120s after restart %s_trade' % item[2])
-                time.sleep(120)
-                is_restart = True
-                logger.info('finish sleep 120s after restart %s_trade' % item[2])
+        cur_timestamp = int(time.time())
+        db_timestamp = query_trade(exchange_id)
+        logger.info('item is: %s, current timestamp:%s (%s) , db timestamp:%s (%s), ' %
+                        (exchange+'_trade', cur_timestamp, format_time(cur_timestamp),
+                            db_timestamp, format_time(db_timestamp)))
+        if cur_timestamp - db_timestamp > 300:
+            restart(exchange + '_trade')
+            logger.info('begin sleep 120s after restart %s_trade' % exchange)
+            time.sleep(120)
+            is_restart = True
+            # logger.info('finish sleep 120s after restart %s_trade' % item[2])
         if not is_restart:
             logger.info('all trade is normal, sleep 30s before next query')
             time.sleep(30)
 
 
 # depth
-def depth_handle():
-    dict1 = dict()
+def depth_handle(exchange_id, exchange, symbol):
     # 保存每个交易所每个交易对的上次查询结果，结果为字典
     result_dict = {}
-    for item in item_list:
-        result_dict[item[1]+'_'+item[2]] = ''
+    result_dict[str(exchange_id)+'_'+symbol] = ''
 
     while True:
         is_restart = False
-        for item in item_list:
-            symbol_rst = dict(query_depth(item[0], item[1]))
-            logger.info('item is: %s,last result is: %s, current result is: %s' %
-                        (str(item[0])+'_'+str(item[1])+'_'+str(item[2]), str(result_dict[item[1]+'_'+item[2]]),
-                         str(symbol_rst)))
-            if cmp(result_dict[item[1]+'_'+item[2]], symbol_rst) == 0:
-                    restart(item[2] + '_depth')
-                    logger.info('begin sleep 120s after restart %s_depth' % item[2])
-                    time.sleep(120)
-                    is_restart = True
-                    logger.info('finish sleep 120s after restart %s_depth' % item[2])
-            result_dict[item[1] + '_' + item[2]] = symbol_rst.copy()
+        symbol_rst = dict(query_depth(exchange_id, symbol))
+        logger.info('item is: %s,last result is: %s, current result is: %s' %
+                    (str(exchange_id)+':'+symbol+':'+exchange, str(result_dict[str(exchange_id)+'_'+symbol]),
+                     str(symbol_rst)))
+        if cmp(result_dict[str(exchange_id)+'_'+symbol], symbol_rst) == 0:
+                restart(exchange + '_depth')
+                logger.info('begin sleep 120s after restart %s_depth' % exchange)
+                time.sleep(120)
+                is_restart = True
+                # logger.info('finish sleep 120s after restart %s_depth' % item[2])
+        result_dict[str(exchange_id)+'_'+symbol] = symbol_rst.copy()
 
         if not is_restart:
-            logger.info('all depth is normal, sleep 30s before next query')
-            time.sleep(30)
+            logger.info('all depth is normal, sleep 300s before next query')
+            time.sleep(300)
 
 
 if __name__ == '__main__':
     cur0 = get_mysqlcur()
-    sql = "SELECT	t1.`exchange_id`,t1.`symbol`,t2.`name` as exchange_name FROM `symbols` t1" \
-        " JOIN `exchanges` t2 ON t1.`exchange_id` =t2.`id` WHERE (t1.`base_currency` ='BTC' AND" \
-        " (t1.`quote_currency`='USD' OR t1.`quote_currency`='USDT'))OR (t1.`base_currency`='ETH' AND " \
-        "t1.`quote_currency`='BTC')"
+    sql = "SELECT `id` ,`name`   FROM `exchanges` "
     cur0.execute(sql)
-    item_list = cur0.fetchall()
+    # 所有的交易所
+    exchange_list = cur0.fetchall()
     cur0.close()
-    # 目前所有交易对
-    # ((1, 'btcusdt', 'huobipro'), (1, 'ethbtc', 'huobipro'), (2, 'tBTCUSD', 'bitfinex'),
-    # (2, 'tETHBTC', 'bitfinex'), (3, 'BTCUSDT', 'binance'), (3, 'ETHBTC', 'binance'))
 
-    ticket_thread = Thread(target=ticker_handle, name='ticker_thread')
-    kline_thread = Thread(target=kline_handle, name='kline_thread')
-    trade_thread = Thread(target=trade_handle, name='trade_thread')
-    depth_thread = Thread(target=depth_handle, name='depth_thread')
-
-    ticket_thread.start()
-    kline_thread.start()
-    trade_thread.start()
-    depth_thread.start()
+    # depth特殊处理,因为部分交易所不支持BTC
+    depth_cur = get_mysqlcur()
+    depth_sql = "SELECT	t1.`exchange_id`,t1.`symbol`,t2.`name_zh`  as exchange_name FROM `symbols` t1" \
+                " JOIN `exchanges` t2 on t1.`exchange_id` =t2.`id` WHERE t1.`base_currency` ='btc' " \
+                " GROUP BY `id` "
+    # 1
+    # btcusdt
+    # 火币pro
+    # 2
+    # tBTCEUR
+    # bitfinex
+    # 3
+    # BTCUSDT
+    # 币安
+    depth_cur.execute(depth_sql)
+    exchange_depth_list = depth_cur.fetchall()
+    depth_cur.close()
+    exchange_in_depth = {}
+    for item in exchange_depth_list:
+        exchange_in_depth[item[0]] = item
 
     logger.info('start monitor daemon')
+    # 共有交易所数*4个线程
+    for item in exchange_list:
+        ticket_thread = Thread(target=ticker_handle, name=item[1] + '_ticker_thread', args=(item[0], item[1]))
+        kline_thread = Thread(target=kline_handle, name=item[1] + '_kline_thread', args=(item[0], item[1]))
+        trade_thread = Thread(target=trade_handle, name=item[1] + '_trade_thread', args=(item[0], item[1]))
+
+        if exchange_in_depth.has_key(item[0]):
+            depth_thread = Thread(target=depth_handle, name=item[1]+'_depth_thread', args=(exchange_in_depth[item[0]][0],
+                                                                                           item[1],
+                                                                                           exchange_in_depth[item[0]][1]))
+        else:
+            sql_temp = "SELECT `exchange_id` ,`symbol` ,SUM(`usd_vol`) usd_vol FROM `market_history_trade` " \
+                  " WHERE `exchange_id` =%s GROUP BY `symbol` ORDER BY amount DESC LIMIT 1"
+            cur_temp = get_mysqlcur()
+            cur_temp.execute(sql_temp, args=(item[0],))
+            res = cur_temp.fetchone()
+            cur_temp.close()
+            depth_thread = Thread(target=depth_handle, name=item[1] + '_depth_thread', args=(res[0], item[1], res[1]))
+
+        ticket_thread.start()
+        kline_thread.start()
+        trade_thread.start()
+        depth_thread.start()
--- z_work/test2.py
+++ z_work/test2.py
@@ -1,16 +1,27 @@
 import MySQLdb
 import logging
+import time
+import redis
 
-logging.basicConfig(level=logging.INFO, filename='monitor_info.log', format=' %(asctime)s - %(levelname)s - %(message)s')
-logging.basicConfig(level=logging.ERROR, filename='monitor_error.log', format=' %(asctime)s - %(levelname)s - %(message)s')
-logging.info('start monitor daemon')
-conn = MySQLdb.connect(host='rm-j6ccb77fan1q3p0n8.mysql.rds.aliyuncs.com',
-                           user='rshvip', passwd='Aa112255', db='coindata')
-cur = conn.cursor()
-sql3 = "SELECT	t1.`exchange_id`,t1.`symbol`,t2.`name` as exchange_name FROM `symbols` t1 JOIN `exchanges` t2 ON t1.`exchange_id` =t2.`id`  WHERE (t1.`base_currency` ='BTC' AND (t1.`quote_currency`='USD' OR t1.`quote_currency`='USDT')) OR (t1.`base_currency`='ETH' AND t1.`quote_currency`='BTC')"
-sql0 = "SELECT `close` FROM  `market_ticker` WHERE `exchange_id` =1 AND `symbol` ='btcusdt'"
-sql = "SELECT `time` FROM `market_history_trade` WHERE `exchange_id`=1 AND `symbol`='btcusdt' ORDER BY `time` DESC LIMIT 1"
-sql4 = "SELECT `time` FROM `market_kline` WHERE `exchange_id`=1 AND `symbol`='btcusdt' AND `period`=1 ORDER BY `time` DESC LIMIT 1"
-sql5 = "SELECT t1.`quote_currency`,t1.`base_currency`,GROUP_CONCAT(t1.`exchange_id` order by t4.`priority`) as exchange_ids,GROUP_CONCAT(t1.`symbol`) as symbols,t2.`priority` as base_priority,t3.`priority` as quote_priority FROM `symbols` t1 JOIN `currency_info` t2 ON t1.base_currency=t2.currency JOIN `currency_info` t3 ON t1.quote_currency=t3.currency JOIN `exchanges` t4 ON t1.`exchange_id` =t4.`id` WHERE t1.base_currency LIKE '%ETH%' GROUP BY t1.quote_currency,t1.base_currency ORDER BY `base_priority`,`quote_priority`"
-cur.execute(sql5)
-print (cur.fetchall())
+# logging.basicConfig(level=logging.INFO, filename='monitor_info.log', format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.ERROR, filename='monitor_error.log', format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.info('start monitor daemon')
+# conn = MySQLdb.connect(host='rm-j6ccb77fan1q3p0n8.mysql.rds.aliyuncs.com',
+#                            user='rshvip', passwd='Aa112255', db='coindata')
+# cur = conn.cursor()
+# sql3 = "SELECT	t1.`exchange_id`,t1.`symbol`,t2.`name` as exchange_name FROM `symbols` t1 JOIN `exchanges` t2 ON t1.`exchange_id` =t2.`id`  WHERE (t1.`base_currency` ='BTC' AND (t1.`quote_currency`='USD' OR t1.`quote_currency`='USDT')) OR (t1.`base_currency`='ETH' AND t1.`quote_currency`='BTC')"
+# sql0 = "SELECT `close` FROM  `market_ticker` WHERE `exchange_id` =1 AND `symbol` ='btcusdt'"
+# sql = "SELECT `time` FROM `market_history_trade` WHERE `exchange_id`=1 AND `symbol`='btcusdt' ORDER BY `time` DESC LIMIT 1"
+# sql4 = "SELECT `time` FROM `market_kline` WHERE `exchange_id`=1 AND `symbol`='btcusdt' AND `period`=1 ORDER BY `time` DESC LIMIT 1"
+# sql5 = "SELECT t1.`quote_currency`,t1.`base_currency`,GROUP_CONCAT(t1.`exchange_id` order by t4.`priority`) as exchange_ids,GROUP_CONCAT(t1.`symbol`) as symbols,t2.`priority` as base_priority,t3.`priority` as quote_priority FROM `symbols` t1 JOIN `currency_info` t2 ON t1.base_currency=t2.currency JOIN `currency_info` t3 ON t1.quote_currency=t3.currency JOIN `exchanges` t4 ON t1.`exchange_id` =t4.`id` WHERE t1.base_currency LIKE '%ETH%' GROUP BY t1.quote_currency,t1.base_currency ORDER BY `base_priority`,`quote_priority`"
+# cur.execute(sql5)
+# print (cur.fetchall())
+print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1530008529))
+# rd = redis.StrictRedis(host='r-j6c9942d609540d4.redis.rds.aliyuncs.com', port=6379, db=0, password='Jr12345678')
+# key = ('4:*')
+# result_dict = rd.hgetall(key)
+# print(result_dict)
+
+重启等待时间
+#  SELECT a.`name_zh` ,b.`time` FROM `exchanges` a JOIN  (SELECT `exchange_id` ,MAX(`time`) time FROM `market`  GROUP BY `exchange_id` ) b ON a.`id` = b.`exchange_id`
+
